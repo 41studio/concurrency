b0a6971dab7d4fbd2fcf0443835d5bb441819bf1
Hendra <hendra@41studio.com>
2019-03-26 21:46:54 +0700

    Update configuration.rb
    added api key support

--- lib/concurrency/configuration.rb
+++ lib/concurrency/configuration.rb
@@ -1,11 +1,11 @@
 module Concurrency
   class Configuration
-    attr_accessor :from_currency, :to_currency
+    attr_accessor :api_key, :from_currency, :to_currency
 
-    def initialize(api_key=ENV['CURRENCY_CONVERTER_API_KEY'], from_currency="USD", to_currency="JPY")
+    def initialize(api_key, from_currency="USD", to_currency="JPY")
+        @api_key = api_key
         @from_currency = from_currency
         @to_currency = to_currency
-        @api_key = api_key
     end
   end
 end
