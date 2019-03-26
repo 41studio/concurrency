79656a8f02f899edc766014b9ebe2092722551d7
Hendra <hendra@41studio.com>
2019-03-26 21:37:15 +0700

    Update configuration.rb
    add currency converter api key

--- lib/concurrency/configuration.rb
+++ lib/concurrency/configuration.rb
@@ -2,9 +2,10 @@ module Concurrency
   class Configuration
     attr_accessor :from_currency, :to_currency
 
-    def initialize
-        @from_currency = "USD"
-        @to_currency = "INR"
+    def initialize(api_key=ENV['CURRENCY_CONVERTER_API_KEY'], from_currency="USD", to_currency="JPY")
+        @from_currency = from_currency
+        @to_currency = to_currency
+        @api_key = api_key
     end
   end
 end
